ce9c1b25da69b17bfa4fe1f23a2040edf14fec43
derrickip34 <derrickip34@gmail.com>
2020-04-25 15:35:10 +0300

    get news sources by category

--- app/requests.py
+++ app/requests.py
@@ -11,3 +11,34 @@ def configure_request(app):
     sources_url = app.config['NEWS_SOURCES_URL']
     articles_url = app.config['NEWS_ARTICLES_URL']
 
+def get_sources(category):
+
+    get_sources_url = sources_url.format(category,api_key)
+
+    with urllib.request.urlopen(get_sources_url) as url:
+        get_sources_data = url.read()
+        get_sources_response = json.loads(get_sources_data)
+
+        sources_results = None
+
+        if get_sources_response['sources']:
+            sources_results = process_results(get_sources_response['sources'])
+
+    return sources_results
+
+def process_results(new_sources_list):
+
+    news_sources_results = []
+
+    for source in new_sources_list:
+        id = source.get('id')
+        name = source.get('name')
+        description = source.get('description')
+        url = source.get('url')
+        category = source.get('category')
+        country = source.get('country')
+        if url:
+            news_source_object = Sources(id,name,description,url,category,country)
+            news_sources_results.append(news_source_object)
+    
+    return news_sources_results
