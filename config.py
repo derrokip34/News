4b14008d6903cfae9a7eb4765c39a2b5e8b14e6f
derrickip34 <derrickip34@gmail.com>
2020-04-25 21:50:45 +0300

    change headlines url

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 class Config:
 	NEWS_ARTICLES_URL='https://newsapi.org/v2/everything?language=en&sources={}&apiKey={}'
 	NEWS_SOURCES_URL='https://newsapi.org/v2/sources?language=en&category={}&apiKey={}'
-	TOP_HEADLINES_BASE_URL = 'https://newsapi.org/v2/top-headlines?sources=bbc-news,bloomberg,engadget,espn,fortune,al-jazeera-english,cnn,independent&pageSize={}&apiKey={}'
+	TOP_HEADLINES_URL = 'https://newsapi.org/v2/top-headlines?sources=bbc-news,bloomberg,engadget,espn,fortune,al-jazeera-english,cnn,independent&pageSize={}&apiKey={}'
 	NEWS_API_KEY =os.environ.get('NEWS_API_KEY')
 
 class ProdConfig(Config):
