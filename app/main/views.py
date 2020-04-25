7e8198980528d6d2673910fb5703125d7f19e5a6
derrickip34 <derrickip34@gmail.com>
2020-04-25 21:11:25 +0300

    add articles view

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,6 @@
 from flask import render_template
 from . import main
-from ..requests import get_sources
+from ..requests import get_sources,get_articles
 from ..models import Sources,Articles
 
 @main.route('/')
@@ -11,7 +11,16 @@ def index():
     sports_news = get_sources('sports')
     technology_news = get_sources('technology')
     business_news = get_sources('business')
+    health_news = get_sources('health')
 
     title = 'Welcome to Fro News'
 
-    return render_template('index.html', title = title,general = general_news, entertainment = entertainment_news, sports = sports_news, technology = technology_news, business = business_news)
+    return render_template('index.html', title = title,general = general_news, entertainment = entertainment_news, sports = sports_news, technology = technology_news, business = business_news, health = health_news)
+
+@main.route('/articles/<id>')
+def show_articles(id):
+
+    news_articles = get_articles(id)
+    title = f'{id}'
+
+    return render_template('articles.html', title = title, news = news_articles)
